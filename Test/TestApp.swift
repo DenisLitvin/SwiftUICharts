10bdbf1588cb5d6e3a37fc8c91b40d8cf2ca147d
Denis Litvin <den.litvinn@gmail.com>
2023-07-10 20:02:09 -0700

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Denis Litvin on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
